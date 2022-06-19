ed34f961843a6572186431d85f3e5d173340519b
Jenifer Mendoza <miau@MacBook-Air-de-Jenifer.local>
2022-06-19 11:48:01 +0200

    First commit

---
+++ src/main/java/com/example/UrlShortAPI/UrlShortApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.UrlShortAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UrlShortAPI/UrlShortApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UrlShortAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
